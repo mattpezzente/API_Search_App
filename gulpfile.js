0aa8e746a72f9c6623e6ccc763defee0f57f9c1e
Matt <matt@Matts-MacBook-Pro-2.local>
2017-09-29 13:13:35 -0400

    80% completion on structure and styling of site

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,39 @@ var sass = require('gulp-sass')
 var browserSync = require('browser-sync').create()
 
 gulp.task('browser-sync', () => {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
 
-gulp.task('reload', (done) => {
-    browserSync.reload();
-    done();
+gulp.task('reload', done => {
+  browserSync.reload();
+  done();
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./index.html', ['reload']);
-    gulp.watch('./js/**', ['reload']);
+  gulp.watch('./*.html', ['reload']);
+  gulp.watch('./*.js', ['reload']);
+  gulp.watch('./js/**', ['reload']);
 });
 
 gulp.task('css', () => {
-    return gulp.src('./sass/*.sass')
-    .pipe(sass())
+  return gulp.src('./sass/*.sass')
+    .pipe(sass().on('error', function(err) {
+      console.error(err.message);
+      browserSync.notify(err.message, 3000); // Display error in the browser
+      this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
+    }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({
-        stream:true
-    }))
+      stream:true
+  }))
 })
 
 gulp.task('default', ['browser-sync', 'css', 'watch'], () => {
-    gulp.watch('./sass/**/*.sass', ['css'])
+  gulp.watch('./sass/**/*.sass', ['css'])
 })
 
 
---
+++ js/main.js
@@ -0,0 +1,17 @@
+var temp = ""
+
+
+GetData => {
+
+}
+
+
+
+
+
+
+temp += '<li>'
+temp += '<img src="/assets/desktop.png" alt="">'
+temp += '<h3>Batman</h3>'
+temp += '<h4>DC Comics</h4>'
+temp += '</li>'
