105f8ca3e6cdfdb7f5f11bdb357e21e1358279b7
Matt <matt@Matts-MacBook-Pro-2.local>
2017-10-02 12:51:56 -0400

    Implemented fallback JS for API connection failures

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # "Pokesearch" API_Search_App
-The **_Pokesearch_** app is a single-page web application that allows the user to input a Pokemon type, 
-and returns 12 Pokemon that match the inputted type.
+The **_Pokesearch_** app is a single-page web application that allows the user to input a Pokemon type, and return 12 Pokemon that match the inputted type.
 
 ### Demo Link
 [Pokemon Search App Demo](http://mattpezzente.com/projects/pokesearch/pokesearch.html)
@@ -9,7 +8,7 @@ and returns 12 Pokemon that match the inputted type.
 ### Notes - 10/01/2017
 * After entering the Pokemon type to search for, be sure to click the search button once.
 * Wait 8 - 12 seconds for the search results to show.
-* If content isn't loading, check the console for errors. More than likely the cors proxy is unable to connect, please message me directly if you experience this issue. [My Profile](https://github.com/salogost)
+* If content isn't loading, check the console for errors. Please message me directly if you experience this issue. [My Profile](https://github.com/salogost)
 
 ## Types
 The useable input types are listed below:
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// const cors = 'https://cors-anywhere.herokuapp.com/';
-// const api = 'http://pokeapi.co/api/v2/';  
-// var limit = 12;
+const cors = 'https://cors-anywhere.herokuapp.com/';
+const api = 'http://pokeapi.co/api/v2/';  
+var limit = 12;
 var h2Results;
 var ulResults;
 var btnSearch;
@@ -8,6 +8,10 @@ var frmSearch;
 var txtSearch;
 var pokeArray;
 var typeArray;
+var cachedPokeArray;
+var cachedTypeArray;
+var inputValue;
+var valid;
 
 (() => {
   h2Results = document.querySelector('#search-results h2');
@@ -17,161 +21,41 @@ var typeArray;
   btnSearch = document.querySelector('.btn-search');
 
   frmSearch.addEventListener('submit', FormStop);
-  btnSearch.addEventListener('click', TypeSearch);
+  btnSearch.addEventListener('click', PokeSearch);
 
-/* Cached API Functions */
-  pokeArray = [];
   fetch('json/pokedex.json')
-    .then(resp => resp.json())
-    .then(resp => {
-      pokeArray = resp;
-    })
-    .catch(error => {
-      console.log('error loading pokedex database, ' + error.message);
-    });
+  .then(resp => resp.json())
+  .then(resp => cachedPokeArray = resp)
+  .catch(error => {
+    console.log('error loading local types database, ' + error.message);
+  });
 
   fetch('json/types.json')
-    .then(resp => resp.json())
-    .then(resp => typeArray = resp)
-    .catch(error => {
-      console.log('error loading types database, ' + error.message);
-    });
-/************************/
-
+  .then(resp => resp.json())
+  .then(resp => cachedTypeArray = resp)
+  .catch(error => {
+    console.log('error loading local types database, ' + error.message);
+  });
 })();
 
-function FormStop(e) {
-  e.preventDefault();
-}
-
-/* Start Cached API Functions */
-
-function TypeSearch(e) {
-  let errorHTML;
-
-  if (frmSearch.querySelector('.search-error') !== null) {
-    frmSearch.querySelector('.search-error').remove();
-  }
-  else {
-    if (frmSearch.querySelector('.search-error') === null) {
-      errorHTML = '<label class="search-error">error, type not found</label>';
-      frmSearch.insertAdjacentHTML('beforeend', errorHTML);
-    }
-  }
-
-  for (var i = 0; i < typeArray.length; i++) {
-    if (txtSearch.value.toLowerCase() === typeArray[i].ename.toLowerCase()) {
-      ulResults.innerHTML = '';
-      DisplayPokemon();
-      if (frmSearch.querySelector('.search-error') !== null) {
-        frmSearch.querySelector('.search-error').remove();
-        break;
-      }
-    }
-    else {
-      if (frmSearch.querySelector('.search-error') === null) {
-        errorHTML = '<label class="search-error">error, type not found</label>';
-        frmSearch.insertAdjacentHTML('beforeend', errorHTML);
-      }
-    }
-  }
-}
-
-function DisplayPokemon() {
-  let tempHTML
-  let tempType1 = txtSearch.value;
-  let count = 0;
-
-  h2Results.innerHTML = 'Search Results For: <span class="type type-' + tempType1 + '">' + tempType1 + '</span>';
-
-  for (let i = 0; i < pokeArray.length; i++) {
-    if (pokeArray[i].type[0].includes(FindCName(tempType1))) {
-      tempHTML = '<li>';
-      tempHTML += '<img src="http://www.pokestadium.com/sprites/xy/' + FormatPokeName(pokeArray[i].ename.toLowerCase()) + '.gif" alt="">';
-      tempHTML += '<h3>' + pokeArray[i].ename.capsFirstLetter() + '</h3>';
-      tempHTML += '<p class="type type-' + tempType1.toLowerCase() + '">' + tempType1.toLowerCase() + '</p>';
-      if (pokeArray[i].type.length > 1) {
-        tempHTML += '<p class="type type-' + FindEName(pokeArray[i].type[1]) + '">' + FindEName(pokeArray[i].type[1]) + '</p>';
-      }
-      tempHTML += '</li>';
-      ulResults.insertAdjacentHTML('beforeend', tempHTML);
-      count++;
-    }
-    else if (pokeArray[i].type.length > 1 && pokeArray[i].type[1].includes(FindCName(tempType1))) {
-      tempHTML = '<li>';
-      tempHTML += '<img src="http://www.pokestadium.com/sprites/xy/' + FormatPokeName(pokeArray[i].ename.toLowerCase()) + '.gif" alt="">';
-      tempHTML += '<h3>' + pokeArray[i].ename.capsFirstLetter() + '</h3>';
-      tempHTML += '<p class="type type-' + tempType1.toLowerCase() + '">' + tempType1.toLowerCase() + '</p>';
-      if (pokeArray[i].type.length > 1) {
-        tempHTML += '<p class="type type-' + FindEName(pokeArray[i].type[0]) + '">' + FindEName(pokeArray[i].type[0]) + '</p>';
-      }
-      tempHTML += '</li>';
-      ulResults.insertAdjacentHTML('beforeend', tempHTML);
-      count++;
-    }
-  }
-  if (count % 3 !== 0) {
-    tempHTML = '<li style="background:none">';
-    tempHTML += '</li>';
-    ulResults.insertAdjacentHTML('beforeend', tempHTML);
-  }
-}
-
-/* End Cached API Functions */
-
-function FormatPokeName(pokeName) {
-  if (pokeName.includes('\'')) {
-    pokeName = pokeName.replace('\'', '');
-  }
-  if (pokeName.includes('.')) {
-    pokeName = pokeName.replace('.', '');
-  }
-  if (pokeName.indexOf(' ') > 0) {
-    pokeName = pokeName.replace(' ', '-');  
-  }
-
-  return pokeName;
-}
-
-function FindEName(cname) {
-  for (let i = 0; i < typeArray.length; i++) {
-    if(cname.toLowerCase() === typeArray[i].cname){
-      return typeArray[i].ename.toLowerCase();
-    }
-  }
-}
-
-function FindCName(ename) {
-  for (let i = 0; i < typeArray.length; i++) {
-    if(ename.toLowerCase() === typeArray[i].ename.toLowerCase()){
-      return typeArray[i].cname;
-    }
-  }
-}
-
-String.prototype.capsFirstLetter = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-};
-
-
-
 /* Start External API Functions */
-/*
 
 function PokeSearch(e) {
-  let txtValue = txtSearch.value;
-  pokeArray = [];
-
-  fetch(cors+api+'type/'+txtValue)
-    .then(resp => resp.json())
-    .then(resp => {
-      pokeArray = resp.pokemon;
-      ulResults.innerHTML = '';
-      TypeSearch();
-    })
-    .catch(error => {
-      console.log('error loading types database, ' + error.message);
-    });
+  inputValue = txtSearch.value;
+  
+  if (ValidateType(inputValue)) {
+    console.log('yup');
+    fetch(cors+api+'type/'+inputValue)
+      .then(resp => resp.json())
+      .then(resp => {
+        pokeArray = resp.pokemon;
+        TypeSearch();
+      })
+      .catch(error => {
+        console.log('error loading Pokemon database, ' + error.message);
+        DisplayCachedPokemon();
+      });
+  }
 }
 
 function TypeSearch() {
@@ -190,20 +74,24 @@ function TypeSearch() {
       })
       .catch(error => {
         console.log('error loading types database, ' + error.message);
+        DisplayCachedPokemon();
       });
   }
 }
 
 function DisplayPokemon() {
+  ulResults.innerHTML = '';
+  h2Results.innerHTML = 'Search Results For: <span class="type type-' + inputValue + '">' + inputValue + '</span>';
+
   for (let i = 0; i < limit; i++) {
     temp = '<li>';
     temp += '<img src="http://www.pokestadium.com/sprites/xy/' + pokeArray[i].pokemon.name + '.gif" alt="">';
     temp += '<h3>' + pokeArray[i].pokemon.name.capsFirstLetter() + '</h3>';
     for (let j = 0; j < typeArray.length; j++) {
       if (typeArray[j].name == pokeArray[i].pokemon.name) {
-          temp += '<p class="type-' + typeArray[j].types[0].type.name + '">' + typeArray[j].types[0].type.name + '</p>';
+          temp += '<p class="type type-' + typeArray[j].types[0].type.name + '">' + typeArray[j].types[0].type.name + '</p>';
         if (typeArray[j].types[1]) {
-          temp += '<p class="type-' + typeArray[j].types[1].type.name + '">' + typeArray[j].types[1].type.name + '</p>';
+          temp += '<p class="type type-' + typeArray[j].types[1].type.name + '">' + typeArray[j].types[1].type.name + '</p>';
         }
       }
     }
@@ -211,6 +99,135 @@ function DisplayPokemon() {
 
     ulResults.insertAdjacentHTML('beforeend', temp);
   }
+  RemoveLoading();
 }
 
 /* End External API Functions */
+
+/* Start Cached API Functions */
+
+function DisplayCachedPokemon() {
+  let tempHTML
+  let count = 0;
+
+  ulResults.innerHTML = '';
+  h2Results.innerHTML = 'Search Results For: <span class="type type-' + inputValue + '">' + inputValue + '</span>';
+
+  for (let i = 0; i < cachedPokeArray.length; i++) {
+    if (cachedPokeArray[i].type[0].includes(FindCName(inputValue))) {
+      tempHTML = '<li>';
+      tempHTML += '<img src="http://www.pokestadium.com/sprites/xy/' + FormatPokeName(cachedPokeArray[i].ename.toLowerCase()) + '.gif" alt="">';
+      tempHTML += '<h3>' + cachedPokeArray[i].ename.capsFirstLetter() + '</h3>';
+      tempHTML += '<p class="type type-' + inputValue.toLowerCase() + '">' + inputValue.toLowerCase() + '</p>';
+      if (cachedPokeArray[i].type.length > 1) {
+        tempHTML += '<p class="type type-' + FindEName(cachedPokeArray[i].type[1]) + '">' + FindEName(cachedPokeArray[i].type[1]) + '</p>';
+      }
+      tempHTML += '</li>';
+      ulResults.insertAdjacentHTML('beforeend', tempHTML);
+      count++;
+    }
+    else if (cachedPokeArray[i].type.length > 1 && cachedPokeArray[i].type[1].includes(FindCName(inputValue))) {
+      tempHTML = '<li>';
+      tempHTML += '<img src="http://www.pokestadium.com/sprites/xy/' + FormatPokeName(cachedPokeArray[i].ename.toLowerCase()) + '.gif" alt="">';
+      tempHTML += '<h3>' + cachedPokeArray[i].ename.capsFirstLetter() + '</h3>';
+      tempHTML += '<p class="type type-' + inputValue.toLowerCase() + '">' + inputValue.toLowerCase() + '</p>';
+      if (cachedPokeArray[i].type.length > 1) {
+        tempHTML += '<p class="type type-' + FindEName(cachedPokeArray[i].type[0]) + '">' + FindEName(cachedPokeArray[i].type[0]) + '</p>';
+      }
+      tempHTML += '</li>';
+      ulResults.insertAdjacentHTML('beforeend', tempHTML);
+      count++;
+    }
+
+    if (i >= limit) {
+      break;
+    }
+
+  }
+  if (count % limit > 0) {
+    tempHTML = '<li style="background:none">';
+    tempHTML += '</li>';
+    ulResults.insertAdjacentHTML('beforeend', tempHTML);
+  }
+  RemoveLoading();
+}
+
+/* End Cached API Functions */
+
+/* Start Utility Functions */
+
+function FormStop(e) {
+  e.preventDefault();
+}
+
+function ValidateType(value) {
+  if (frmSearch.querySelector('.search-error') !== null) {
+    frmSearch.querySelector('.search-error').remove();
+  }
+  if (document.querySelector('.loading') === null) {
+    h2Results.insertAdjacentHTML('afterend', '<img class="loading" src="/assets/load.gif">');
+  }
+  for (let i = 0; i < cachedTypeArray.length; i++) {
+    if (value.toLowerCase() === cachedTypeArray[i].ename.toLowerCase()) {
+      if (frmSearch.querySelector('.search-error') !== null) {
+        frmSearch.querySelector('.search-error').remove();
+      }
+      valid = true;
+      break;
+    }
+    else {
+      valid = false;
+    }
+  }
+  if(valid === false && frmSearch.querySelector('.search-error') === null) {
+    errorHTML = '<label class="search-error">error, type not found</label>';
+    frmSearch.insertAdjacentHTML('beforeend', errorHTML);
+    RemoveLoading();
+  }
+
+  return valid;
+}
+
+function RemoveLoading() {
+  if (document.querySelector('.loading')) {
+    document.querySelector('.loading').remove();
+  }
+}
+
+function FormatPokeName(pokeName) {
+  if (pokeName.includes('\'')) {
+    pokeName = pokeName.replace('\'', '');
+  }
+  if (pokeName.includes('.')) {
+    pokeName = pokeName.replace('.', '');
+  }
+  if (pokeName.indexOf(' ') > 0) {
+    pokeName = pokeName.replace(' ', '-');  
+  }
+
+  return pokeName;
+}
+
+function FindEName(cname) {
+  for (let i = 0; i < cachedTypeArray.length; i++) {
+    if(cname.toLowerCase() === cachedTypeArray[i].cname){
+      return cachedTypeArray[i].ename.toLowerCase();
+    }
+  }
+}
+
+function FindCName(ename) {
+  for (let i = 0; i < cachedTypeArray.length; i++) {
+    if(ename.toLowerCase() === cachedTypeArray[i].ename.toLowerCase()){
+      return cachedTypeArray[i].cname;
+    }
+  }
+}
+
+String.prototype.capsFirstLetter = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
+/* End Utility Functions */
+
+
